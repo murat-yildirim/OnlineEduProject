c6ceb102d0d63d65d20c184067462bd1c83eaad6
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-04-23 21:25:57 +0300

    7 ve 8.Ders Sonu

---
+++ OnlineEdu.DTO/DTOs/BlogCategoryDtos/CreateBlogCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using OnlineEdu.DTO.DTOs.BlogDtos;
+using OnlineEdu.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DTO.DTOs.BlogCategoryDtos
+{
+    public class CreateBlogCategoryDto
+    {
+        public string Name { get; set; }
+
+        public List<ResultBlogDto> Blogs { get; set; }
+    }
+}
