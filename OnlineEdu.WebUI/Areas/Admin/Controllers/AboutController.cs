e85e9db9cebc270dfe9baff4b3b7d56f76547113
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-04-28 23:14:06 +0300

    UI Katmanında DTO'ların oluşturulması tamamlandı

--- OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -2,11 +2,13 @@
 
 namespace OnlineEdu.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     [Route("[area]/[controller]/[action]/{id?}")]
-    public class AboutController : Controller
+    public class AboutController(HttpClient _client) : Controller
     {
         public IActionResult Index()
         {
+            //var values = _client.GetFromJsonAsync
             return View();
         }
     }
