f64259ae6b0fc3d21cc9ce8c405c86da3c7d1f67
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-05-18 00:41:18 +0300

    Register teması ve custom error işlemleri tamamlandı

--- OnlineEdu.WebUI/Controllers/RegisterController.cs
+++ OnlineEdu.WebUI/Controllers/RegisterController.cs
@@ -15,14 +15,15 @@ namespace OnlineEdu.WebUI.Controllers
         public async Task<IActionResult> Signup(UserRegisterDto userRegisterDto)
         {
             var result = await _userService.CreateUserAsync(userRegisterDto);
-            if(!result.Succeeded)
+            if(!result.Succeeded || !ModelState.IsValid)
             {
                 foreach (var item in result.Errors)
                 {
-                    ModelState.AddModelError(item.Code, item.Description);
+                    ModelState.AddModelError("", item.Description);
                 }
                 return View();
             }
+
             return RedirectToAction("Index", "Login");
         }
     }
