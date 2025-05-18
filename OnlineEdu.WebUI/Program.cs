7a73d5f145f29f3f00f59f2a48cb0f3b78bfad24
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-05-18 23:27:49 +0300

    Role service ve role controller tamamlandı mapping işlemi yapıldı

--- OnlineEdu.WebUI/Program.cs
+++ OnlineEdu.WebUI/Program.cs
@@ -2,13 +2,17 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using OnlineEdu.DataAccess.Context;
 using OnlineEdu.Entity.Entities;
+using OnlineEdu.WebUI.Services.RoleServices;
 using OnlineEdu.WebUI.Services.UserServices;
 using OnlineEdu.WebUI.Validators;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IUserServices, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddDbContext<OnlineEduContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
