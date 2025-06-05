8d533f4b46e3ef2bc6d4f0f82841a0d13f152dd9
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-06 01:01:32 +0300

    Admin ve UI tarafında referanslar alanı tamamlandı

---
+++ OnlineEdu.WebUI/ViewComponents/Home/_HomeTestimonialComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using OnlineEdu.WebUI.DTOs.TestimonialDtos;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.Home
+{
+    public class _HomeTestimonialComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultTestimonialDto>>("testimonials");
+            return View(values);
+        }
+    }
+
+}
