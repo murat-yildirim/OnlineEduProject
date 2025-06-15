b0c3a476ee1aa634ff1afc9534a61f1b6c1e1d2a
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-15 22:36:18 +0300

    İletişim sayfası mesaj gönderme işlemi tamamlandı

---
+++ OnlineEdu.WebUI/ViewComponents/UILayout/_LayoutSubscribeComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.ViewComponents.UILayout
+{
+    public class _LayoutSubscribeComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
