4b632e8fb78126ae8175548288f06bb0725c427b
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-16 22:40:11 +0300

    404 ve 403 hata sayfaları tamamlandı

--- OnlineEdu.WebUI/Program.cs
+++ OnlineEdu.WebUI/Program.cs
@@ -23,6 +23,7 @@ builder.Services.ConfigureApplicationCookie(cfg =>
 {
     cfg.LoginPath = "/Login/SignIn";
     cfg.LogoutPath = "/Login/SignOut";
+    cfg.AccessDeniedPath = "/ErrorPage/AccessDenied";
 
 });
 builder.Services.AddControllersWithViews();
@@ -41,6 +42,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseStatusCodePagesWithReExecute("/ErrorPage/NotFound404");
 app.UseAuthentication();
 app.UseAuthorization();
 
