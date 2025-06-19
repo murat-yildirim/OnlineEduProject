55dc23e8853fe0f6f443b674697fdc2545f86eef
Murat Yıldırım <30393559+murat-yildirim@users.noreply.github.com>
2025-06-19 21:29:58 +0300

    CourseVideo entity ve api endpointleri tamamlandı

---
+++ OnlineEdu.API/Mapping/CourseVideoMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OnlineEdu.DTO.DTOs.CourseCategoryDtos;
+using OnlineEdu.DTO.DTOs.CourseVideoDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Mapping
+{
+    public class CourseVideoMapping : Profile
+    {
+        public CourseVideoMapping()
+        {
+            CreateMap<CourseVideo,CreateCourseVideoDto>().ReverseMap();
+            CreateMap<CourseVideo,UpdateCourseVideoDto>().ReverseMap();
+            CreateMap<CourseVideo,ResultCourseVideoDto>().ReverseMap();
+        }
+    }
+}
--- OnlineEdu.Entity/Entities/Course.cs
+++ OnlineEdu.Entity/Entities/Course.cs
@@ -23,6 +23,7 @@ namespace OnlineEdu.Entity.Entities
         public AppUser AppUser { get; set; }
 
         public List<CourseRegister> CourseRegisters { get; set; }
+        public List<CourseVideo> CourseVideos { get; set; }
 
     }
 }
